a57ae443aa632798a647196c44d311f2ebfd364d
AymanAttili <aymanayman2622002@gmail.com>
2023-05-10 23:49:31 +0300

    Done

--- myJS.js
+++ myJS.js
@@ -1,3 +1,13 @@
+let back=document.querySelector('.back');
+
+back.addEventListener('mouseenter',function(){
+    back.src='./SVG/back1.svg';
+})
+back.addEventListener('mouseleave',function(){
+    back.src='./SVG/back.svg';
+})
+
+
 function changeVis(){
     let input = document.getElementsByClassName('sign_password');
     let icons = document.getElementsByClassName('eye');
@@ -24,22 +34,63 @@ function visEye(id){
 }
 
 let owner = document.getElementById('owner');
+let ownerIcon = document.getElementById('ownerIcon')
 let tenant = document.getElementById('tenant');
+let tenantIcon = document.getElementById('tenantIcon');
 
-function flip(id){
-    if(id==='owner'){
+let type;
+
+owner.addEventListener('mouseenter', function(){
+    owner.style.color='#EC801D';
+    owner.style.background='unset';
+    owner.style.border='3.5px solid #EC801D'
+    ownerIcon.src='./SVG/owner.svg'
+
+})
+owner.addEventListener('mouseleave', function(){
+    if(type!=='owner'){
+        owner.style.color='white';
+        owner.style.background='#EC801D';
+        ownerIcon.src='./SVG/owner1.svg'
+    }
+})
+owner.addEventListener('click', function(){
+    owner.style.color='#EC801D';
+    owner.style.background='unset';
+    owner.style.border='3.5px solid #EC801D'
+    ownerIcon.src='./SVG/owner.svg'
+    type='owner';
+
+    tenant.style.color='white';
+    tenant.style.background='#09203F';
+    tenantIcon.src='./SVG/tenant1.svg';
+})
+
+
+
+
+tenant.addEventListener('mouseenter', function(){
+    tenant.style.color='#09203F';
+    tenant.style.background='unset';
+    tenant.style.border='3.5px solid #09203F'
+    tenantIcon.src='./SVG/tenant.svg'
+
+})
+tenant.addEventListener('mouseleave', function(){
+    if(type!=='tenant'){
         tenant.style.color='white';
         tenant.style.background='#09203F';
-        tenant.src='./SVG/owner.svg';
-
-        
-        
-        
-        owner.style.color= '#EC801D';
-        owner.style.background= 'unset';
-        owner.style.border='3.5px solid var(--secondary-color)'; 
+        tenantIcon.src='./SVG/tenant1.svg'
     }
-    else{
+})
+tenant.addEventListener('click', function(){
+    tenant.style.color='#09203F';
+    tenant.style.background='unset';
+    tenant.style.border='3.5px solid #09203F'
+    tenantIcon.src='./SVG/tenant.svg'
+    type='tenant';
 
-    }
-}
+    owner.style.color='white';
+    owner.style.background='#EC801D';
+    ownerIcon.src='./SVG/owner1.svg';
+})
