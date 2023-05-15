116a40a497b4c6b29d22580e7cb0d8302c62b725
SaifAbuR3d <saefraed2@gmail.com>
2023-05-16 01:36:32 +0300

    Almost done
    next is to classify customers into two tables

--- Signup.php
+++ Signup.php
@@ -5,7 +5,7 @@ include("database.php");
 
 <!DOCTYPE html>
 <html lang="en">
-    <!-- testing git -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -37,7 +37,7 @@ include("database.php");
     <div class="sign_screen">
         <div class="container">
 
-            <form class="sign_form" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+            <form class="sign_form" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
 
                 <h1>Create an account</h1>
 
@@ -82,7 +82,7 @@ include("database.php");
                         Tenatnt <img id="tenantIcon" class="btnIcon" src="./SVG/tenant1.svg">
                     </button>
 
-                    <button type="button" id="owner" value ="owner" class="typeButton owner">
+                    <button type="button" id="owner" value="owner" class="typeButton owner">
                         Owner <img id="ownerIcon" class="btnIcon" src="./SVG/owner1.svg">
                     </button>
                 </div>
@@ -91,7 +91,7 @@ include("database.php");
             </form>
 
             <div class="others">
-    
+
                 <p>Do you have an account? <a href="./Login.php">Login</a> <br><br><br>
                     or continue with:
                 </p>
@@ -120,5 +120,97 @@ include("database.php");
 </html>
 
 <?php
+
+function phpAlert($msg)
+{
+    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+}
+
+function password_strength($password) {
+	if ( !preg_match("#[0-9]+#", $password) )
+        return False;
+	if ( !preg_match("#[a-z]+#", $password) )
+        return False;
+	if ( !preg_match("#[A-Z]+#", $password) )
+        return False;
+
+	return True;
+}
+
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve the form data
+    $email = $_POST["email"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $uname = $_POST["uname"];
+    $password = $_POST["password"];
+    $repassword = $_POST["repassword"];
+    // $userType = $_POST["userType"];     // to see later
+
+
+    // Sanitize -> cause a problem in password 
+    // $email = filter_input(INPUT_POST, $email, FILTER_SANITIZE_SPECIAL_CHARS);
+    // $fname = filter_input(INPUT_POST, $email, FILTER_SANITIZE_SPECIAL_CHARS);
+    // $lname = filter_input(INPUT_POST, $email, FILTER_SANITIZE_SPECIAL_CHARS);
+    // $password = filter_input(INPUT_POST, $email, FILTER_SANITIZE_SPECIAL_CHARS);
+    // $repassword = filter_input(INPUT_POST, $email, FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+
+    // Validate
+    if ($password !== $repassword) {
+        phpAlert("Passwords do not match");
+        exit;
+    }
+
+    if (strlen($_POST["password"]) < 8) {
+        phpAlert("Password must be at least 8 characters");
+        exit;
+    }
+
+    if (!password_strength($password)) {
+        phpAlert("Password must contain at least one capital letter, one small letter, and one number");
+        exit;
+    }
+
+
+    /// 
+
+
+    // Check if the username or email already exists in the database
+    $query = "SELECT * FROM customer WHERE User_name ='$uname' OR Email = '$email'";
+    $result = mysqli_query($conn, $query);
+
+    // Check if any rows were returned
+    if (mysqli_num_rows($result) > 0) {
+        // User already exists
+        phpAlert("Username or email already taken");
+        exit;
+    }
+    //User Data is Valid. 
+
+    // Hash the password, for security requirements. 
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    //Store in Database
+    $query = "INSERT INTO customer (User_name , Email, First_name, Last_name, Password)
+                             VALUES ('$uname', '$email', '$fname', '$lname', '$hashed_password')";
+    mysqli_query($conn, $query);                          
+
+    // Testing 
+
+    if (mysqli_affected_rows($conn)) {
+        phpAlert("User registered successfully");
+        // redirect to login page 
+        // header("Location: login.php");
+        exit;
+    } else {
+        phpAlert("Error: User registration failed");
+        exit; 
+    }
+}
+
+
 mysqli_close($conn);
 ?>
--- database.php
+++ database.php
@@ -4,10 +4,12 @@ $db_user = "root";
 $db_pass = "";
 $db_name ="rentalmate";
 
-// Create connection
-$conn = mysqli_connect($db_server, $db_user, $db_pass,$db_name);
+$conn = mysqli_connect($db_server,
+                       $db_user,
+                       $db_pass,
+                       $db_name);
+                       
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
