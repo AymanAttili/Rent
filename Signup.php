e75757b7a2fdb5038ad663b1d92bf955a9e8be98
SaifAbuR3d <saefraed2@gmail.com>
2023-05-15 23:29:57 +0300

    :)
    Connecting to Database. With few edits to Form

--- Signup.php
+++ Signup.php
@@ -1,12 +1,18 @@
+<?php
+include("database.php");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!--Main template CSS file-->
     <link rel="stylesheet" href="./CSS/myStyle.css" />
 
-<!--    Google fonts-->
+    <!--    Google fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
@@ -15,7 +21,7 @@
 </head>
 
 <body>
-    
+
     <!-- start header -->
     <header class="header">
         <div class="container">
@@ -30,84 +36,89 @@
     <!-- start sign_screen -->
     <div class="sign_screen">
         <div class="container">
-        
-            <form class="sign_form" action="">
+
+            <form class="sign_form" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
 
                 <h1>Create an account</h1>
 
                 <fieldset class="sign_field">
-                    <legend class="sign_legend" >Email address</legend>
-                    <input class="sign_input" type="email">
+                    <legend class="sign_legend">Email address</legend>
+                    <input class="sign_input" type="email" name="email" required>
                 </fieldset>
 
                 <div class="name">
                     <fieldset class="sign_field">
-                        <legend class="sign_legend" >First name</legend>
-                        <input class="sign_input" type="text">
+                        <legend class="sign_legend">First name</legend>
+                        <input class="sign_input" type="text" name="fname" required>
                     </fieldset>
 
-                <fieldset class="sign_field">
-                    <legend class="sign_legend" >Last name</legend>
-                    <input class="sign_input" type="text">
-                </fieldset>
+                    <fieldset class="sign_field">
+                        <legend class="sign_legend">Last name</legend>
+                        <input class="sign_input" type="text" name="lname" required>
+                    </fieldset>
                 </div>
 
                 <fieldset class="sign_field">
-                    <legend class="sign_legend" >Username</legend>
-                    <input class="sign_input" type="text">
+                    <legend class="sign_legend">Username</legend>
+                    <input class="sign_input" type="text" name=uname required>
                 </fieldset>
 
 
                 <fieldset class="sign_field">
-                    <legend class="sign_legend" >Password</legend>
-                    <input class="sign_password" type="password" onfocus="visEye(eye2)" >
+                    <legend class="sign_legend">Password</legend>
+                    <input class="sign_password" type="password" name="password" required onfocus="visEye(eye2)">
                     <img class="eye" id="eye2" src="./SVG/ic_baseline-remove-red-eye.svg" onclick="changeVis()">
                 </fieldset>
 
                 <fieldset class="sign_field">
-                    <legend class="sign_legend" >Confirm password</legend>
-                    <input class="sign_password" type="password" onfocus="visEye(eye3)" >
+                    <legend class="sign_legend">Confirm password</legend>
+                    <input class="sign_password" type="password" name="repassword" required onfocus="visEye(eye3)">
                     <img class="eye" id="eye3" src="./SVG/ic_baseline-remove-red-eye.svg" onclick="changeVis()">
                 </fieldset>
-                
+
                 <div class="userType">
                     <p>Select User type:</p>
-                    <button type="button" id="tenant" class="typeButton tenant"  >
+                    <button type="button" id="tenant" value="tenant" class="typeButton tenant">
                         Tenatnt <img id="tenantIcon" class="btnIcon" src="./SVG/tenant1.svg">
                     </button>
 
-                    <button type="button" id="owner" class="typeButton owner"  >
+                    <button type="button" id="owner" value ="owner" class="typeButton owner">
                         Owner <img id="ownerIcon" class="btnIcon" src="./SVG/owner1.svg">
                     </button>
                 </div>
-                <button type="submit">Continue</button> 
+                <button type="submit" value="submit">Continue</button>
 
             </form>
 
             <div class="others">
-    
+
                 <p>Do you have an account? <a href="./Login.html">Login</a> <br><br><br>
                     or continue with:
-                </p>  
+                </p>
                 <img src="./SVG/ic_baseline-apple.svg">
                 <img src="./SVG/ant-design_facebook-filled.svg">
                 <img src="./SVG/akar-icons_google-contained-fill.svg">
             </div>
-    </div>
-    <!-- end sign_screen -->
+        </div>
+        <!-- end sign_screen -->
 
 
-    <div class="hid" style="visibility: hidden; height: 40px;">
-        HHHHHHHHHHHHHHHHHH
-    </div>
+        <div class="hid" style="visibility: hidden; height: 40px;">
+            HHHHHHHHHHHHHHHHHH
+        </div>
 
 
-    <div class="footer">
-        &copy; 2023 <span>RentalMate</span> All Right Reserved
-    </div>
-    
-    <script src="./JS/myJS.js">
-    </script>
-    
+        <div class="footer">
+            &copy; 2023 <span>RentalMate</span> All Right Reserved
+        </div>
+
+        <script src="./JS/myJS.js">
+        </script>
+
 </body>
+
 </html>
+
+<?php
+mysqli_close($conn);
+?>
