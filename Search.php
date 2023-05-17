58eb72197a08f8201b5579f34cd76d0ea6aea71d
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2023-05-17 21:01:18 +0300

    cleaning myRealty

--- Search.php
+++ Search.php
@@ -185,27 +185,7 @@
 
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUxruZQ8r_AszFmZvHCH_hlDrUL3_plRE"></script>
     
-    <script>
-        
-        function initialize(x, y){
-            let myCenter = new google.maps.LatLng(x, y);
-            let mapProp = {
-                center:myCenter,
-                zoom:17,
-                mapTypeId:'hybrid'
-            };
-        
-            let map = new google.maps.Map(document.getElementById("map"),mapProp);
-        
-            let marker = new google.maps.Marker({
-                position:myCenter,
-                icon: './SVG/map_marker.svg'
-            });
-        
-            marker.setMap(map);
-        }
-        google.maps.event.addDomListener(window, 'load', initialize(32.304956, 35.038719));
-    </script>
+    <script src="./JS/searchMap.js"></script>
     </section>
     <!-- End body -->
 
--- myRealty.php
+++ myRealty.php
@@ -1,3 +1,8 @@
+<?php
+include("database.php");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,90 +13,88 @@
   <link rel="stylesheet" href="CSS/bootstrap.css">
   <link rel="stylesheet" href="./CSS/myRealty.css">
   <link rel="stylesheet" href="./CSS/navbar.css">
-  <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <title>Profile</title>
 </head>
 
 <body>
 
-  <?php    include('./Templates/navbar.php'); ?>
+  <?php include('./Templates/navbar.php'); ?>
 
 
   <!------------------------------------------END HEADER---------------------------------------->
   <section>
     <div class="container">
-      <h1 >Add Property</h1>
-      <div class="row justify-content-center">
-
-        <div class="left-side col-12 col-lg-6">
-          <div class="row justify-content-center align-items-center" style="margin-top: 10px;">
-            <label class="col-12 col-md-3 text-center" for="">Location</label>
-            <input class="col-4 mx-2" placeholder="Lat" type="number" id="lat">
-            <input class="col-4" placeholder="Lng" type="number" id="lng">
-          </div>
-
-          <div class="mapContainer col-12" style="margin-top: 10px; margin-bottom: 10px;">
-            <a id="getBtn" class="direction-link" target="_blank"
-              > Get Directions</a>
-            <div id="map"></div>
-
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUxruZQ8r_AszFmZvHCH_hlDrUL3_plRE"></script>
-
-            <script src="./JS/addMap.js"> </script>
-          </div>
-        </div>
-        <div class="right-side row col-12 col-lg-6">
-          <div class="information  row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Image</label>
-            <input class="col-7 col-md-8" style="background-color: white; border: none;" type="file"
-              accept="image/png, image/jpeg, image/jpg" multiple>
-          </div>
-          <hr>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Price</label>
-            <input class="col-7 col-md-8" type="number">
-          </div>
-          <hr>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Country</label>
-            <input class="col-7 col-md-8" type="text">
-          </div>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">City</label>
-            <input class="col-7 col-md-8" type="text">
-          </div>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Street</label>
-            <input class="col-7 col-md-8" type="text">
-          </div>
-          <hr>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Description</label>
-            <input class="col-7 col-md-8" type="text">
-          </div>
-          <hr>
-          <div class="information row align-items-center">
-            <label class="col-5 col-md-4 text-center" for="">Beds</label>
-            <input class="col-7 col-md-8" type="number">
-          </div>
-          <hr>
-          <div class="information row align-items-center">
-            <label class="text-center" style="display: block;" for="">Date</label>
-            <div class="row align-items-center">
-              <label class="col-5 col-md-4 text-center" for="">From</label>
-              <input class="col-7 col-md-8" type="date">
+      <h1>Add Property</h1>
+      <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data" method="POST">
+        <div class="row justify-content-center">
+          <div class="left-side col-12 col-lg-6">
+            <div class="row justify-content-center align-items-center" style="margin-top: 10px;">
+              <label class="col-12 col-md-3 text-center" for="">Location</label>
+              <input class="col-4 mx-2" placeholder="Lat" type="number" id="lat" name="lat">
+              <input class="col-4" placeholder="Lng" type="number" id="lng" name="lng">
             </div>
-            <div class="row align-items-center">
-              <label class="col-5 col-md-4 text-center" for="">To</label>
-              <input class="col-7 col-md-8" type="date">
+
+            <div class="mapContainer col-12" style="margin-top: 10px; margin-bottom: 10px;">
+              <a id="getBtn" class="direction-link" target="_blank"> Get Directions</a>
+              <div id="map"></div>
+
+              <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUxruZQ8r_AszFmZvHCH_hlDrUL3_plRE"></script>
+
+              <script src="./js/addMap.js"></script>
             </div>
           </div>
-        </div>
+          <div class="right-side row col-12 col-lg-6">
+            <div class="information  row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Image</label>
+              <input class="col-7 col-md-8" style="background-color: white; border: none;" type="file" accept="image/png, image/jpeg, image/jpg" name="image" required>
+            </div>
+            <hr>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Price</label>
+              <input class="col-7 col-md-8" type="number" name="price" required>
+            </div>
+            <hr>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Country</label>
+              <input class="col-7 col-md-8" type="text" name="country" required>
+            </div>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">City</label>
+              <input class="col-7 col-md-8" type="text" name="city" required>
+            </div>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Street</label>
+              <input class="col-7 col-md-8" type="text" name="street">
+            </div>
+            <hr>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Description</label>
+              <input class="col-7 col-md-8" type="text" name="description">
+            </div>
+            <hr>
+            <div class="information row align-items-center">
+              <label class="col-5 col-md-4 text-center" for="">Beds</label>
+              <input class="col-7 col-md-8" type="number" name="beds" required>
+            </div>
+            <hr>
+            <div class="information row align-items-center">
+              <label class="text-center" style="display: block;" for="">Date</label>
+              <div class="row align-items-center">
+                <label class="col-5 col-md-4 text-center" for="">From</label>
+                <input class="col-7 col-md-8" type="date" name="from_date" required>
+              </div>
+              <div class="row align-items-center">
+                <label class="col-5 col-md-4 text-center" for="">To</label>
+                <input class="col-7 col-md-8" type="date" name="to_date" required>
+              </div>
+            </div>
+          </div>
 
-        <button class="col-12 Add-prop text-center" type="submit">Add Property</button>
-      </div>
+          <button class="col-12 Add-prop text-center" type="submit" name="submit">Add Property</button>
+        </div>
+      </form>
     </div>
   </section>
 
@@ -125,8 +128,6 @@
   <script src="./js/jquery-3.6.4.min.js"></script>
   <script src="./js/bootstrap.js"></script>
   <script src="./js/browse.js"></script>
-
-<script>
 
 </body>
 
