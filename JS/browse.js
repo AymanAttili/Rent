f60784ab539793b4372661735d29151ef41c5866
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2023-05-18 23:43:06 +0300

    like icon change

--- JS/browse.js
+++ JS/browse.js
@@ -13,15 +13,11 @@ function popup(margin){
 function xchange(id){
     let like = document.getElementById(id);
     console.log(like + "222");
-    if(like.src=='./SVG/heart2.svg'){
-        like.src='./SVG/heart1.svg'
+    if(like.src.indexOf("heart1.svg") != -1){
+        like.src='./SVG/heart2.svg';
     }
     else{
-        like.src='./SVG/heart2.svg'
+        like.src='./SVG/heart1.svg';
     }
-
-    
 }
-
-
 
