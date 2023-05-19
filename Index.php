e105e93fc3ce8eed7a2f576a7988dc02b0c8e766
SaifAbuR3d <saefraed2@gmail.com>
2023-05-19 19:10:16 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,14 +15,12 @@
     <title>Home</title>
 
 </head>
+
 <body>
 
-    <?php    include('./Templates/navbar.php'); ?>
+    <?php include('./Templates/navbar.php'); ?>
 
-
-
-
-        <section class="d-flex" style=" background-image: url(./img/background1.jpg); 
+    <section class="d-flex" style=" background-image: url(./img/background1.jpg); 
                      background-size: cover; 
                      background-position: center;
                      width: 100%;
@@ -50,93 +49,61 @@
         </div>
     </section>
 
-   <!-- Start cards -->
-
+    <!-- Start cards -->
     <section style="margin-top: 30px; padding: 20px;">
 
         <h1>Hot Properties</h1>
-    
+
+        <?php
+        // get the 6 last inserted properties 
+        $query = "SELECT * FROM property LIMIT 6";
+        $result = mysqli_query($conn, $query);
+        ?>
+
         <div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
-            <div class="card">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
+            <?php
+            while ($row = mysqli_fetch_array($result)) {
+                $image_path = $row['Image_path'];
+                $price = $row['Price'];
+                $beds = $row['Beds'];
+                $location_id = $row['Location_id'];
+                $owner_user_name = $row['Owner_user_name'];
+
+                // get the Country and City from location table
+                $query_location = "SELECT * FROM location WHERE Location_id = '$location_id'";
+                $result_location = mysqli_query($conn, $query_location);
+                $row_location = mysqli_fetch_array($result_location);
+                $country = $row_location['Country'];
+                $city = $row_location['City'];
+
+                // get the owner full name from customer table
+                $query_owner = "SELECT * FROM customer WHERE User_name = '$owner_user_name'";
+                $result_owner = mysqli_query($conn, $query_owner);
+                $row_owner = mysqli_fetch_array($result_owner);
+                $first_name = $row_owner['First_name'];
+                $last_name = $row_owner['Last_name'];
+            ?>
+                <div class="card">
+                    <img class="card-image" src=<?php echo ($image_path) ?>>
+                    <div class="Description">
+                        <div class="content">
+                            <p><?php echo $price . "$" ?></p>
+                            <p><?php echo $first_name . " " . $last_name ?></p>
+                            <p><?php echo $city . ", " . $country ?></p>
+                            <p><?php echo 'Beds: ' . $beds ?></p>
+                        </div>
                     </div>
+
                 </div>
-                
-            </div>
-            <div class="card ">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="card ">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="card ">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="card ">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="card ">
-                <img class="card-image" src="img/backiee-176475.jpg">
-                <div class="Description">
-                    <div class="content">
-                        <p>$3,000+</p>
-                        <p>Sabastea, Nablus</p>
-                        <p>El baeader, Opposite Rasim coffe</p>
-                        <p>2-3 beds • 2 Bath</p>
-                    </div>
-                </div>
-                
-            </div>
-        </div> 
-        
+            <?php } ?>
+        </div>
+
     </section>
 
-        <!-- End cards -->
-        
-    
-        
+    <!-- End cards -->
+
+
+
 
     <script src="./JS/browse.js"></script>
 
