ec734f90279ddc94c6b9ba42503d6c3c850e8550
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2023-05-21 02:54:25 +0300

    add footer to signup page

--- Signup.php
+++ Signup.php
@@ -127,6 +127,10 @@ include("database.php");
         <div class="hid" style="visibility: hidden; height: 40px;">
             HHHHHHHHHHHHHHHHHH
         </div>
+
+        <footer class="footer">
+            &copy; 2023 <span>RentalMate</span> All Right Reserved
+        </footer>
         <script src="./JS/myJS.js"></script>
 </body>
 
